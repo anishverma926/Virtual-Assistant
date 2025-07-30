020865254a15f1f3528f5eb5898563679671ad2d
anishverma926 <167166559+anishverma926@users.noreply.github.com>
2025-07-31 00:35:21 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -11,7 +11,7 @@ dotenv.config()
 
 const app = express();
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://virtual-assistant-frontend-v9dw.onrender.com",
     credentials: true
 }))
 const port = process.env.PORT || 5000
