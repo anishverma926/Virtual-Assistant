16bbb067c3513bdb10513f6790f666adbe858179
anishverma926 <167166559+anishverma926@users.noreply.github.com>
2025-07-31 00:27:27 +0530

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -5,7 +5,7 @@ import SignIn from './pages/SignIn'
 import Customize from './pages/Customize'
 import { useState } from 'react'
 import { useContext } from 'react'
-import { userDataContext } from './context/userContext'
+import { userDataContext } from './context/UserContext'
 import Home from './pages/Home'
 import Customize2 from './pages/Customize2'
 
